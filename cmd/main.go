923261f5d7df505816c0ca5e6128e8ec4720e7e7
JP-93 <pedrobsouza03@gmail.com>
2023-07-09 22:52:41 -0300

    api

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"desafio-genesisbank/database"
 	"desafio-genesisbank/handler"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,8 +11,10 @@ import (
 
 func main() {
 	database.ConnectionDB()
+	fmt.Println("API-Cotação is running....")
 	r := chi.NewRouter()
 	r.Get("/exchange/{amout}/{from}/{to}/{rate}", handler.ExchangeValue)
+	r.Get("/exchanges", handler.GetAllValues)
 	http.ListenAndServe(":8000", r)
 
 }
